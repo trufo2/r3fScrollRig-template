cb1335af3e30892697492da238d6a1bbcc86e2e1
trufo2 <trufo2@gmail.com>
2025-03-14 23:09:33 +0100

    fix: update SVG asset paths to be relative for GitHub Pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
 						className='text-neutral-400 hover:text-white focus-outline-none sm:hidden flex'
 						aria-label='Toggle menu'>
 						<img
-							src={isOpen ? "/assets/close.svg" : "/assets/menu.svg"}
+							src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
 							alt='toggle'
 							className='w-6 h-6'
 						/>
